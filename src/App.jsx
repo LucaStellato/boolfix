32f32b66d9396c46b689f00b74044068cc6488d4
LucaStellato <lucastellato435@gmail.com>
2025-11-19 12:59:02 +0100

    create endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
+  const endpointKey = import.meta.env.VITE_API_KEY;
+  const [film, setFilm] = useState([]);
+  const endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${endpointKey}`
   return (
     <>
 
